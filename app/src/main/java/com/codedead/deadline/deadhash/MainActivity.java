e3a4557c5735bfdfbf9c481c6194b4aac54136e4
CodeDead <admin@codedead.com>
2017-05-04 09:39:09 +0200

    * Strings placed in resources for future multilanguage support * Changed navigation items

--- app/src/main/java/com/codedead/deadline/deadhash/MainActivity.java
+++ app/src/main/java/com/codedead/deadline/deadhash/MainActivity.java
@@ -80,17 +80,17 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_file) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_text) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_compare) {
 
         } else if (id == R.id.nav_manage) {
 
-        } else if (id == R.id.nav_share) {
+        } else if (id == R.id.nav_help) {
 
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_about) {
 
         }
 
